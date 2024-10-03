528702e481712426e11c341dc32f1256a25a3ee8
Bi54rq <159131675+Bi54rq@users.noreply.github.com>
2024-10-03 15:58:00 +0100

    wall jump new movement scr

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,3 +1,4 @@
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,14 +11,18 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
 	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
-	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+	[SerializeField] private Collider2D m_CrouchDisableCollider;
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+
+
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
+
+
 
 	[Header("Events")]
 	[Space]
@@ -132,7 +137,8 @@ public class CharacterController2D : MonoBehaviour
 		}
 	}
 
-
+	
+	
 	private void Flip()
 	{
 		// Switch the way the player is labelled as facing.
